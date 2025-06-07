743ae1043fd8aa9234bd9207c7f75084e163967f
Fantix King <fantix.king@gmail.com>
2025-06-07 15:36:05 -0400

    Use new-style license header for newly-added files

--- gel/auth/builtin_ui.py
+++ gel/auth/builtin_ui.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright Gel Data Inc. and the contributors.
 
-
 from gel._internal._auth._builtin_ui import (
     BuiltinUIResponse,
     BuiltinUI,
--- gel/auth/email_password.py
+++ gel/auth/email_password.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright Gel Data Inc. and the contributors.
 
-
 from gel._internal._auth._email_password import (
     SignUpCompleteResponse,
     SignUpVerificationRequiredResponse,
